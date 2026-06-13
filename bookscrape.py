471d28d9afb236ef5edbcdf0c6fc26bed2b7db71
JMotonu <mtnjosephat@gmail.com>
2026-06-13 13:47:51 +0300

    add pandas and save to csv or xlsx functionality

--- bookscrape.py
+++ bookscrape.py
@@ -2,6 +2,8 @@ import requests
 
 from bs4 import BeautifulSoup
 
+import pandas as pd 
+
 url = "http://books.toscrape.com/index.html"
 
 response = requests.get(url)
@@ -30,12 +32,16 @@ for card in cards:
     # create a dictionary to hold the title and price of each book
     book_item = {
         "title": title,
-        "price": price
+        "price": price.replace("£", "") # remove the pound sign from the price. method chaining is when we call multiple methods on the same object. In this case, we are calling the replace method on the price string.
     }
     all_scraped_books.append(book_item)
 
-print(all_scraped_books)
+#print(all_scraped_books)
 
+# pandas is a library that allows us to create a dataframe, which is a table that can hold our data in a structured way
 
+df = pd.DataFrame(all_scraped_books)    # pd will 
 
+#df.to_csv("scraped_books.csv", index=False) # index=False means we don't want to include the index column in the csv file
 
+df.to_excel("scraped_books.xlsx", index=False) # we can also save the dataframe as an excel file. we need to install the openpyxl library to do this. pip install openpyxl
